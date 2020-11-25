9b58179a3d5a9a26b4721cafc96e7a0933793010
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-25 10:49:23 -0300

    fourth commit

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const PORT = process.env.PORT || 4000;
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-
-const cors = require("cors")
+const path = require("path");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("public"))
-    // app.use(express.static(path.join(__dirname, 'build')));
+    /* app.use(express.static(path.join(__dirname, 'public'))); */
 }
 
 app.listen(PORT, () => console.log("hi, your port is ", PORT))
