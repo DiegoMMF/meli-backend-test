3d1750c4bc3c18d38d9d3a8ed85c9b4674406fef
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-25 11:06:16 -0300

    get for all

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use("/api", routes);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("public"))
     /* app.use(express.static(path.join(__dirname, 'public'))); */
+
 }
 
 app.listen(PORT, () => console.log("hi, your port is ", PORT))
