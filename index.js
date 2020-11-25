421e88ab679dbdb67500a69e805d14b935a76a1a
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-25 11:10:46 -0300

    ...

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use("/api", routes);
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 /* if (process.env.NODE_ENV === "production") {
